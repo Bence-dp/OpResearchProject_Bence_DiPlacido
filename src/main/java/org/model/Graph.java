6d584fe22f015265194020fec107ac56ab0d703f
Bence-dp <bencediplacido16@gmail.com>
2026-04-13 17:19:28 +0200

    graph structure

--- src/main/java/org/model/Graph.java
+++ src/main/java/org/model/Graph.java
@@ -1,5 +1,50 @@
 package org.model;
 
-public class Graph {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Graph {
+    private StartNode startNode;
+    private EndNode endNode;
+    private List<Node> nodeList;
+
+    /**
+     * @param startNode
+     * @param endNode
+     * @param nodeList
+     */
+    public Graph(StartNode startNode, EndNode endNode, List<Node> nodeList) {
+        this.startNode = startNode;
+        this.endNode = endNode;
+        this.nodeList = nodeList;
+    }
+
+    public StartNode getStartNode() {
+        return startNode;
+    }
+
+    public void setStartNode(StartNode startNode) {
+        this.startNode = startNode;
+    }
+
+    public EndNode getEndNode() {
+        return endNode;
+    }
+
+    public void setEndNode(EndNode endNode) {
+        this.endNode = endNode;
+    }
+
+    public List<Node> getNodeList() {
+        return nodeList;
+    }
+
+    public void setNodeList(List<Node> nodeList) {
+        this.nodeList = nodeList;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
 }
